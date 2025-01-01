f539db3b58fec255d04d399e91c964593aace542
Mankaran Singh <singmank@amdocs.com>
2025-01-01 23:09:28 +0530

    Added: T&C, PP pages

---
+++ src/components/Details/MapComponent.tsx
@@ -0,0 +1,304 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2>Privacy Policy</h2>
+      <div>
+        <p>
+          Grabbzo Enterprises Private Limited and/or its affiliates (“Grabbzo”,
+          the “company’, “we”, “us” and “our”) respect your privacy and is
+          committed to protecting it through its compliance with its privacy
+          policies. This policy describes:
+        </p>
+        <ul>
+          <li>
+            the types of information that Grabbzo may collect from you when you
+            access or use its websites, applications and other online services
+            (collectively, referred as "Services"); and
+          </li>
+          <li>
+            its practices for collecting, using, maintaining, protecting and
+            disclosing that information.
+          </li>
+        </ul>
+        <p>
+          This policy applies only to the information Grabbzo collects through
+          its Services, in email, text and other electronic communications sent
+          through or in connection with its Services.
+        </p>
+        <p>
+          This policy DOES NOT apply to information that you provide to, or that
+          is collected by, any third-party, such as restaurants at which you
+          make reservations and/or pay through Grabbzo’s Services and social
+          networks that you use in connection with its Services. Grabbzo
+          encourages you to consult directly with such third-parties about their
+          privacy practices.
+        </p>
+        <p>
+          Please read this policy carefully to understand Grabbzo's policies and
+          practices regarding your information and how Grabbzo will treat it. By
+          accessing or using its Services and/or registering for an account with
+          Grabbzo, you agree to this privacy policy and you are consenting to
+          Grabbzo's collection, use, disclosure, retention, and protection of
+          your personal information as described here. If you do not provide the
+          information Grabbzo requires, Grabbzo may not be able to provide all
+          of its Services to you.
+        </p>
+        <p>
+          If you reside in India or any other part of the world, Grabbzo
+          Enterprises Private Limited, having it’s registered office H.No. 30,
+          Jain Nagar, Talab Tilloo, Jammu, 180002, India will be the controller
+          of your personal data provided to, or collected by or for, or
+          processed in connection with our Services.
+        </p>
+        <p>
+          Your data controller is responsible for the collection, use,
+          disclosure, retention, and protection of your personal information in
+          accordance with its privacy standards as well as any applicable
+          national laws. Your data controller may transfer data to other members
+          of Grabbzo as described in this Privacy Policy. Grabbzo may process
+          and retain your personal information on its servers in India where its
+          data centres are located, and/or on the servers of its third parties
+          (in or outside India), having contractual relationships with Grabbzo.
+        </p>
+        <p>
+          This policy may change from time to time, your continued use of
+          Grabbzo's Services after it makes any change is deemed to be
+          acceptance of those changes, so please check the policy periodically
+          for updates.
+        </p>
+        <h6>The information we collect and how we use it </h6>
+        <p>
+          Grabbzo Limited ("Grabbzo," the "Company," "we," "us," and "our”)
+          collects several types of information from and about users of our
+          Services, including:
+        </p>
+        <p>
+          We collect several types of information from and about users of our
+          Services, including:
+        </p>
+        <ul>
+          <li>
+            <strong>Your Personal Information ("PI"):</strong> Personal
+            Information is the information that can be associated with a
+            specific person and could be used to identify that specific person
+            whether from that data, or from the data and other information that
+            we have, or is likely to have access to. We do not consider personal
+            information to include information that has been made anonymous or
+            aggregated so that it can no longer be used to identify a specific
+            person, whether in combination with other information or otherwise.
+          </li>
+          <li>
+            <strong>
+              Information about your internet connection, the equipment you use
+              to access our Services and your usage details.
+            </strong>
+          </li>
+        </ul>
+        <p>We collect this information:</p>
+        <ul>
+          <li>directly from you when you provide it to us; and/or</li>
+          <li>
+            automatically as you navigate through our Services (information
+            collected automatically may include usage details, IP addresses and
+            information collected through cookies, web beacons and other
+            tracking technologies).
+          </li>
+        </ul>
+        <h2>Information You Provide to Us</h2>
+        <p>
+          The information we collect on or through our Services may include:
+        </p>
+        <ul>
+          <li>
+            <strong>Your account information:</strong> Your full name, email
+            address, postal code, password and other information you may provide
+            with your account, such as your gender, mobile phone number and
+            website. Your profile picture that will be publicly displayed as
+            part of your account profile. You may optionally provide us with
+            this information through third-party sign-in services such as
+            Facebook and Google Plus. In such cases, we fetch and store whatever
+            information is made available to us by you through these sign-in
+            services.
+          </li>
+          <li>
+            <strong>Your preferences:</strong> Your preferences and settings
+            such as time zone and language.
+          </li>
+          <li>
+            <strong>Your content:</strong> Information you provide through our
+            Services, including your reviews, photographs, comments, lists,
+            followers, the users you follow, current and prior restaurant
+            reservation details, food ordering details and history, favorite
+            restaurants, special restaurant requests, contact information of
+            people you add to, or notify of, your restaurant reservations
+            through our Services, names, and other information you provide on
+            our Services, and other information in your account profile.
+          </li>
+          <li>
+            <strong>Your searches and other activities:</strong> The search
+            terms you have looked up and results you selected.
+          </li>
+          <li>
+            <strong>Your browsing information:</strong> How long you used our
+            Services and which features you used; the ads you clicked on.
+          </li>
+          <li>
+            <strong>Your communications:</strong> Communications between you and
+            other users or merchants through our Services; your participation in
+            a survey, poll, sweepstakes, contest or promotion scheme; your
+            request for certain features (e.g., newsletters, updates or other
+            products); your communication with us about employment opportunities
+            posted to the services.
+          </li>
+          <li>
+            <strong>Your transactional information:</strong> If you make
+            reservations or purchases through our Services, we may collect and
+            store information about you to process your requests and
+            automatically complete forms for future transactions, including (but
+            not limited to) your phone number, address, email, billing
+            information and credit or payment card information. This information
+            may be shared with third-parties which assist in processing and
+            fulfilling your requests, including PCI compliant payment gateway
+            processors. When you submit credit or payment card information, we
+            encrypt the information using industry standard technologies, as
+            further described below under "Payment Card Information." If you
+            write reviews about businesses with which you conduct transactions
+            through our Services, we may publicly display information that you
+            transacted with those businesses.
+          </li>
+          <li>
+            <strong>Your Public Posts:</strong> You also may provide information
+            (such as ratings, reviews, tips, photos, comments, likes, bookmarks,
+            friends, lists, etc.) to be published or displayed (hereinafter,
+            "posted") on publicly accessible areas of our Services, or
+            transmitted to other users of our Services or third-parties
+            (collectively, "User Contributions"). Your User Contributions are
+            posted on and transmitted to others at your own risk. Although we
+            limit access to certain pages, you may set certain privacy settings
+            for such information by logging into your account profile. Please be
+            aware that no security measures are perfect or impenetrable (see
+            "Security" section below). Additionally, we cannot control the
+            actions of other users of our Services with whom you may choose to
+            share your User Contributions. Therefore, we cannot and do not
+            guarantee that your User Contributions will not be viewed by
+            unauthorized persons. We may display this information on the
+            Services, share it with businesses, and further distribute it to a
+            wider audience through third party sites and services. You should be
+            careful about revealing any sensitive details about yourself in such
+            postings.
+          </li>
+        </ul>
+        <p>
+          We use the information you provide to us to enhance the functionality
+          and improve the quality of our Services, and to personalize your
+          experience while using our Services. We also use this information to
+          display relevant advertising, provide support to you, communicate with
+          you, and comply with our legal obligations.
+        </p>
+        <h2>Information about Your Friends</h2>
+        <p>
+          You have the option to request your friends to join the Services by
+          providing their contact information. If you request a friend to join
+          and connect with you on the Services, we will only use your friend's
+          contact information to process your request.
+        </p>
+        <h2>Information about Your Messages</h2>
+        <p>
+          If you exchange messages with others through the Services, we may
+          store them in order to process and deliver them, allow you to manage
+          them, and investigate possible violations of our Terms of Service and
+          wrongdoing in connection with the Services. If you send information
+          from the Services to your mobile device via SMS text message, we may
+          log your phone number, phone carrier, and the date and time that the
+          message was processed. Carriers may charge recipients for texts that
+          they receive.
+        </p>
+        <h2>
+          Information We Collect Through Automatic Data Collection Technologies
+        </h2>
+        <p>
+          We may automatically collect certain information about the computer or
+          devices (including mobile devices) you use to access the Services, and
+          about your use of the Services, even if you use the Services without
+          registering or logging in.
+        </p>
+        <ul>
+          <li>
+            <strong>Usage information:</strong> Details of your use of our
+            Services, including traffic data, location data, logs and other
+            communication data and the resources that you access and use on or
+            through our Services.
+          </li>
+          <li>
+            <strong>Computer and device information:</strong> Information about
+            your computer, Internet connection and mobile device, including your
+            IP address, operating systems, platforms, browser type, other
+            browsing information (connection, speed, connection type etc.),
+            device type, device's unique device identifier, mobile network
+            information and the device's telephone number.
+          </li>
+          <li>
+            <strong>Stored information and files:</strong> Our applications also
+            may access metadata and other information associated with other
+            files stored on your mobile device. This may include, for example,
+            photographs, audio and video clips, personal contacts and address
+            book information.
+          </li>
+          <li>
+            <strong>Location information:</strong> Our applications collect
+            real-time information about the location of your device, as
+            permitted by you.
+          </li>
+          <li>
+            <strong>Last URL visited:</strong> The URL of the last web page you
+            visited before visiting our websites.
+          </li>
+          <li>
+            <strong>Mobile device IDs:</strong> Unique mobile device identifier
+            (e.g. IDFA or other device IDs on Apple devices like the iPhone and
+            iPad), if you're using our Services on a mobile device, we may use
+            mobile device IDs (the unique identifier assigned to a device by the
+            manufacturer), instead of cookies, to recognize you. We may do this
+            to store your preferences and track your use of our applications.
+            Unlike cookies, mobile device IDs cannot be deleted. Advertising
+            companies may use device IDs to track your use of our applications,
+            track the number of advertisements displayed, measure advertising
+            performance and display advertisements that are more relevant to
+            you. Analytics companies may use mobile device IDs to track your
+            usage of our applications.
+          </li>
+          <li>
+            <strong>Your preferences:</strong> Your preferences and settings
+            such as time zone and language.
+          </li>
+          <li>
+            <strong>Your activity on the Services:</strong> Information about
+            your activity on the Services, such as your search queries,
+            comments, domain names, search results selected, number of clicks,
+            pages viewed and the order of those pages, how long you visited our
+            Services, the date and time you used the Services, error logs, and
+            other similar information.
+          </li>
+          <li>
+            <strong>Mobile status:</strong> For mobile application users, the
+            online or offline status of your application.
+          </li>
+          <li>
+            <strong>Applications:</strong> : If you use the Grabbzo application,
+            Grabbzo may collect information about the presence and/ or absence
+            and/ or details pertaining to other applications on your mobile
+            phone. The applications we gather information for, may vary across
+            categories including, without limitation, shopping, fashion, food
+            and travel. This will help us understand you and your preferences
+            better and enable Grabbzo to provide you with a personalized
+            experience.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default page;
