bb43243c9375e240ff83180992d9a435087fafe8
Arnav khajuria <arnavk1802@gmail.com>
2025-07-31 19:25:53 +0530

    order history + env changes

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Details/Header";
 import Crown from "@/components/Icons/Crown";
 import CrownMobile from "@/components/Icons/CrownMobile";
 import Faq from "@/components/login/Faq";
@@ -9,9 +10,11 @@ import React from "react";
 const Page = () => {
   return (
     <div className=" pb-10">
+      <Header/>
       <h1 className="hidden">Pricing Page</h1>
       {/* <Header /> */}
       <div className="flex flex-col items-center justify-center my-8 font-poppins text-center relative pt-24">
+        
         <div className="relative">
           <Crown className="absolute -top-28 -left-28 rotate-[-20deg] max-sm:hidden" />
           <CrownMobile className="absolute -top-14 -left-8 rotate-[-20deg] sm:hidden block"/>
